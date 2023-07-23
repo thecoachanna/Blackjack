0efa8d03b2dab62809a39a387abe9393ec6e88be
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2023-07-23 18:32:37 -0400

    Sum and Cards being displayed on screen

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 let firstCard = 7;
-let secondCard = 15;
+let secondCard = 10;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-  message = "Woohoo! You've got Blackjack! 🥳";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() {
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  
 }
 
-// CASH OUT
-console.log(message)
