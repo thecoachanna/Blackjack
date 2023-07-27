89d817359d79dd16253984b93f0df49044f976e0
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2023-07-27 13:51:28 -0400

    Added a player object

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const player = {
+  name: "Anna",
+  chips: 20
+}
+
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,6 +12,9 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
